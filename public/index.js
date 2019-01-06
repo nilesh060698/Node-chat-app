099fce4520b25e7b2200b56a52d5de613c31bc15
coolnilesh <nilesht461@gmail.com>
2019-01-06 18:03:27 +0530

    added create msg and new msg

---
+++ public/index.js
@@ -0,0 +1,26 @@
+
+  var socket=io();
+  socket.on('connect',()=>{
+
+   console.log("connected ton server");
+   socket.emit('createdMsg',{
+       To:'Aman',
+       text:'hi aman how are you',
+       createdAt:123
+   });
+
+
+
+  });
+
+
+
+  socket.on('disconnect',()=>{
+  console.log("server disconnected");
+
+  });
+
+  socket.on('newMsg',(msg)=>{
+    console.log('new msg',msg)
+
+  });
